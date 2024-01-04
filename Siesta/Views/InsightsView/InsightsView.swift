d28aeb55d22cb3b224152eb35c2661ec673421c6
Sanaa Shahzadi <shahzadi.sanaa@gmail.com>
2024-01-04 22:46:43 +0000

    Capturing the users daily highest score. Grouping this based on the score amount into PieChartData. Showing a basic Pie Chart. Needs cleaning up - but has reduce, dictionary to array, array to dictionary conversion notes.

--- Siesta/Views/InsightsView/InsightsView.swift
+++ Siesta/Views/InsightsView/InsightsView.swift
@@ -6,14 +6,32 @@
 //
 
 import SwiftUI
+import Charts
 
 struct InsightsView: View {
+    @EnvironmentObject var viewModel: ViewModel
+    
     
     var body: some View {
         VStack {
             Text("")
                 .navigationTitle("Insights")
                 .navigationBarTitleDisplayMode(.inline)
+            
+            var data = viewModel.pieChartData
+            
+            Chart(data, id: \.id) { element in
+                SectorMark(angle:
+                        .value("Total", element.total),
+                           angularInset: 2.0
+                )
+                .foregroundStyle(by: .value("Group", element.total))
+                .annotation(position: .overlay) {
+                    Text("\(element.group)")
+                        .font(.headline)
+                        .foregroundStyle(.white)
+                }
+            }
         
         }
     }
